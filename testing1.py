781bf7619f61a6eeba482a1ef962dcf6eb6907de
DaiSwap <19644pranavvenkatesh@gmail.com>
2024-05-26 11:54:49 +0530

    Initial commit of tic-tac-toe simple program

---
+++ testing1.py
@@ -0,0 +1,67 @@
+import random
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def check_winner(board, player):
+    win_conditions = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[2][0], board[1][1], board[0][2]],
+    ]
+    return [player, player, player] in win_conditions
+
+def get_available_moves(board):
+    moves = []
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == " ":
+                moves.append((i, j))
+    return moves
+
+def make_move(board, move, player):
+    board[move[0]][move[1]] = player
+
+def get_ai_move(board):
+    available_moves = get_available_moves(board)
+    return random.choice(available_moves)
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        if current_player == "X":
+            row = int(input("Enter the row (0, 1, 2): "))
+            col = int(input("Enter the column (0, 1, 2): "))
+            if (row, col) not in get_available_moves(board):
+                print("Invalid move. Try again.")
+                continue
+            make_move(board, (row, col), current_player)
+        else:
+            print("AI is making a move...")
+            move = get_ai_move(board)
+            make_move(board, move, current_player)
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+
+        if not get_available_moves(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+if __name__ == "__main__":
+    main()
